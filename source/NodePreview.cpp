820cc449dd23afbe1dac6d2818ea1e914d39b202
zz <zhuguang@ejoy.com>
2019-04-13 16:30:38 +0800

    [FXIED] render context

--- source/NodePreview.cpp
+++ source/NodePreview.cpp
@@ -5,6 +5,7 @@
 
 #include <SM_Matrix2D.h>
 #include <unirender/Blackboard.h>
+#include <unirender/RenderContext.h>
 #include <painting2/Blackboard.h>
 #include <painting2/RenderContext.h>
 #include <painting2/RenderTarget.h>
@@ -30,57 +31,42 @@ NodePreview::NodePreview(const Node& node)
 
 void NodePreview::Draw(const n2::RenderParams& rp) const
 {
-    if (!m_draw_list) {
+    if (!m_draw_list || !m_node.GetRGNode()) {
         return;
     }
 
-    // draw to rt
-
     auto& rt_mgr = pt2::Blackboard::Instance()->GetRenderContext().GetRTMgr();
 
+    // draw to rt
     auto rt = rt_mgr.Fetch();
     rt->Bind();
-    //{
-    //    pt2::WindowCtxRegion wcr(static_cast<float>(rt_mgr.WIDTH), static_cast<float>(rt_mgr.HEIGHT));
-
-    //    auto& rc = ur::Blackboard::Instance()->GetRenderContext();
-    //    m_draw_list->Draw(rc);
-    //    m_node.PreviewDraw(m_rg_node, rp);
-    //}
-//    {
-//        auto& rc = ur::Blackboard::Instance()->GetRenderContext();
-////        pt2::RenderTargetCtx ctx(rc, nullptr, rt_mgr.WIDTH, rt_mgr.HEIGHT);
-//
-//        auto wc = std::make_shared<pt2::WindowContext>(rt_mgr.WIDTH, rt_mgr.HEIGHT, rt_mgr.WIDTH, rt_mgr.HEIGHT);
-//        rp::RenderMgr::Instance()->BindWndCtx2D(wc);
-//        wc->Bind();
-//
-//        m_draw_list->Draw(rc);
-//        m_node.PreviewDraw(m_rg_node, rp);
-//
-//        rp::RenderMgr::Instance()->UnbindWndCtx2D(wc);
-//        auto& wc_ori = pt2::Blackboard::Instance()->GetWindowContext();
-//        wc_ori->Bind();
-//    }
     {
         auto& rc = ur::Blackboard::Instance()->GetRenderContext();
         auto renderer = rp::RenderMgr::Instance()->GetRenderer(rp::RenderType::SPRITE);
         auto shader = std::static_pointer_cast<pt2::Shader>(renderer->GetAllShaders()[0]);
         pt2::RenderTargetCtx ctx(rc, shader, rt_mgr.WIDTH, rt_mgr.HEIGHT);
 
+        rp::RenderMgr::Instance()->SetRenderer(rp::RenderType::EXTERN);
+
+        int shader_id = rc.GetBindedShader();
+        size_t rt_depth = rc.GetRenderTargetDepth();
         m_draw_list->Draw(rg::RenderContext(rc));
+        size_t rt_depth2 = rc.GetRenderTargetDepth();
+        for (int i = 0; i < rt_depth2 - rt_depth; ++i) {
+            rc.UnbindRenderTarget();
+        }
+        rc.BindShader(shader_id);
 
         sm::Matrix2D mat;
         mat.Scale(
             static_cast<float>(rt_mgr.WIDTH),
             static_cast<float>(rt_mgr.HEIGHT)
         );
-        m_node.PreviewDraw(m_rg_node, mat);
+        m_node.PreviewDraw(m_node.GetRGNode(), mat);
     }
     rt->Unbind();
 
     // draw rt to screen
-
     auto mt4 = sm::mat4(bp::NodeHelper::CalcPreviewMat(m_node, rp.GetMatrix()));
     const auto scale = mt4.GetScale();
     const float hw = scale.x * 0.5f;
@@ -105,9 +91,12 @@ void NodePreview::Draw(const n2::RenderParams& rp) const
 
 void NodePreview::Update()
 {
-    m_rg.ClearCache();
-    m_rg_node = m_rg.CreateGraphNode(m_node);
-    m_draw_list = std::make_shared<rg::DrawList>(m_rg_node);
+    if (!m_draw_list) {
+        auto& rg_node = m_node.GetRGNode();
+        if (rg_node) {
+            m_draw_list = std::make_shared<rg::DrawList>(rg_node);
+        }
+    }
 }
 
 }

874ecb18185af0bc816abba69e73a68053a77937
zz <xzrunner@gmail.com>
2019-12-02 08:01:46 +0800

    rename

--- source/Node.cpp
+++ source/Node.cpp
@@ -68,7 +68,7 @@ void Node::InitPins(const std::string& name)
 		&& var_exports.is_type<std::vector<rg::Node::Port>>());
 	auto& exports = var_exports.get_value<std::vector<rg::Node::Port>>();
 
-	auto rg2grp = [](std::vector<PinDesc>& dst, const std::vector<rg::Node::Port>& src)
+	auto port_back2front = [](std::vector<PinDesc>& dst, const std::vector<rg::Node::Port>& src)
 	{
 		dst.reserve(dst.size() + src.size());
 		for (int i = 0, n = src.size(); i < n; ++i)
@@ -84,8 +84,8 @@ void Node::InitPins(const std::string& name)
 	};
 
 	std::vector<PinDesc> input, output;
-	rg2grp(input, imports);
-	rg2grp(output, exports);
+	port_back2front(input, imports);
+	port_back2front(output, exports);
 
 	InitPins(input, output);
 }
